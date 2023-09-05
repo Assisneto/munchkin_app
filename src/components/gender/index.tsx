8b2f0c3f137c8aedcddf1ee115dc913164c007f0
Assisneto Damasceno <netodamasceno12@gmail.com>
2023-09-05 16:34:26 -0300

    improve gender icon component

--- src/components/gender/index.tsx
+++ src/components/gender/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 import { Icons } from "./styles";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
 
 type Props = {
   gender: string;
+  style?: StyleProp<ViewStyle | TextStyle>;
+  size?: number;
 };
 
-export const Gender = ({ gender }: Props) => {
-  if (gender === "male") return <Icons name="gender-male" size={18} />;
-  return <Icons name="gender-female" size={18} />;
+export const Gender = ({ gender, style, size }: Props) => {
+  const iconSize = size ? size : 18;
+  if (gender === "male")
+    return <Icons name="gender-male" size={iconSize} style={style} />;
+  return <Icons name="gender-female" size={iconSize} style={style} />;
 };
