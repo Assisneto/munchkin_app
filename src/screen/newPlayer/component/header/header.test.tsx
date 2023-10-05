4ce773c2835579a6e23e9b7f5c21ad6b290d550b
Assisneto Damasceno <netodamasceno12@gmail.com>
2023-10-05 18:42:52 -0300

    add newPlayer header test

---
+++ src/screen/newPlayer/component/header/header.test.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import { Header } from ".";
+import { ThemeProvider } from "styled-components/native";
+import { themes } from "./../../../../theme/theme";
+
+const mockNavigation = {
+  goBack: jest.fn()
+};
+
+jest.mock("@react-navigation/native", () => ({
+  useNavigation: () => mockNavigation
+}));
+
+describe("<Header />", () => {
+  const mockProps = {
+    savePlayer: jest.fn(),
+    showError: jest.fn()
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("renders the title correctly", () => {
+    const { getByText } = render(
+      <ThemeProvider theme={themes.dark}>
+        <Header {...mockProps} />
+      </ThemeProvider>
+    );
+
+    expect(getByText("Novo Munchkin")).toBeTruthy();
+  });
+
+  it("navigates back when the back button is pressed", () => {
+    const { getByTestId } = render(
+      <ThemeProvider theme={themes.dark}>
+        <Header {...mockProps} />
+      </ThemeProvider>
+    );
+
+    const backButton = getByTestId("back-button");
+    fireEvent.press(backButton);
+
+    expect(mockNavigation.goBack).toHaveBeenCalled();
+  });
+
+  it("calls savePlayer when the save button is pressed", () => {
+    const { getByTestId } = render(
+      <ThemeProvider theme={themes.dark}>
+        <Header {...mockProps} />
+      </ThemeProvider>
+    );
+
+    const saveButton = getByTestId("save-button");
+    fireEvent.press(saveButton);
+
+    expect(mockProps.savePlayer).toHaveBeenCalled();
+  });
+});
--- src/screen/newPlayer/component/header/index.tsx
+++ src/screen/newPlayer/component/header/index.tsx
@@ -4,25 +4,24 @@ import { TouchableOpacity } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 
-export const Header = ({
-  savePlayer,
-  showError
-}: {
-  savePlayer: () => Promise<void>;
-  showError: (error: string) => void;
-}) => {
+export const Header = ({ savePlayer }: { savePlayer: () => Promise<void> }) => {
   const navigation = useNavigation();
 
   return (
     <>
       <Container>
         <AlignWrapper>
-          <Icons name="chevron-left" size={34} onPress={navigation.goBack} />
+          <Icons
+            name="chevron-left"
+            testID="back-button"
+            size={34}
+            onPress={navigation.goBack}
+          />
           <Title>Novo Munchkin</Title>
         </AlignWrapper>
         <Options>
           <TouchableOpacity onPress={savePlayer}>
-            <Icons name="check" size={30} />
+            <Icons name="check" testID="save-button" size={30} />
           </TouchableOpacity>
         </Options>
       </Container>
