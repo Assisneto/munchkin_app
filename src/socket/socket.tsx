1c47cc1575e2407c08d5d329656cb3624463c15b
Assisneto Damasceno <netodamasceno12@gmail.com>
2023-11-01 14:38:56 -0300

    refactor socket provider and remove socket hook

--- src/socket/socket.tsx
+++ src/socket/socket.tsx
@@ -1,33 +1,87 @@
-import React, { createContext, useState, useEffect } from "react";
-import { saveSocketType, getSocketType, SocketType } from "../storage/socket";
+import React, { createContext, useEffect, useRef, useState } from "react";
+import { Channel, Socket } from "phoenix";
+import { getRoomID } from "../storage/room";
 
-export const SocketContext = createContext({
-  socketState: SocketType.CLIENT,
-  setSocketState: (value: SocketType) => {}
+interface SocketContextType {
+  socket: Socket | null;
+  channel: Channel | null;
+  setRoomID: (newRoomID: string | null) => void;
+  roomID: string | null;
+}
+
+export const SocketContext = createContext<SocketContextType>({
+  socket: null,
+  channel: null,
+  setRoomID: () => {},
+  roomID: null
 });
 
 export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({
   children
 }) => {
-  const [socketState, setSocketState] = useState(SocketType.CLIENT);
+  const socketRef = useRef<Socket | null>(null);
+  const channelRef = useRef<Channel | null>(null);
+  const [_, setRender] = useState(false);
+  const [roomID, setRoomIDState] = useState<string | null>(null);
+
+  const setRoomID = (newRoomID: string | null) => {
+    setRoomIDState(newRoomID);
+  };
 
   useEffect(() => {
-    (async () => {
-      const storedSocketType = await getSocketType();
-      if (storedSocketType) {
-        setSocketState(storedSocketType);
-      }
-    })();
+    const fetchRoomID = async () => {
+      const id = await getRoomID();
+      setRoomIDState(id);
+    };
+
+    fetchRoomID();
   }, []);
 
-  const toggleSocketState = async (value: SocketType) => {
-    await saveSocketType(value);
-    setSocketState(value);
-  };
+  useEffect(() => {
+    if (roomID === null) {
+      return;
+    }
+    const ws = new Socket(
+      `ws://${process.env.EXPO_PUBLIC_SOCKET_URL}/socket` || "",
+      {}
+    );
+
+    ws.onOpen(() => console.log("Connected."));
+    ws.onError((event) => console.log("Cannot connect.", event));
+    ws.onClose((event) => console.log("Goodbye.", event));
+    ws.connect();
+    socketRef.current = ws;
+
+    const ch = ws.channel(`room:${roomID}`, {});
+
+    ch.join()
+      .receive("ok", (resp) => {
+        console.log("Joined successfully", resp);
+      })
+      .receive("error", (resp) => {
+        console.log("Unable to join", resp);
+      });
+
+    channelRef.current = ch;
+
+    setRender((prevState) => !prevState);
+
+    return () => {
+      ch.leave();
+      if (ws) {
+        ws.disconnect();
+      }
+    };
+  }, [roomID]);
 
   return (
     <SocketContext.Provider
-      value={{ socketState, setSocketState: toggleSocketState }}
+      value={{
+        socket: socketRef.current,
+        channel: channelRef.current,
+        setRoomID,
+        roomID
+      }}
     >
       {children}
     </SocketContext.Provider>
