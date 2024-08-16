a4d80b6fef885697da19dc64d9ab56f178234bae
Assisneto Damasceno <netodamasceno12@gmail.com>
2024-08-16 02:35:55 -0300

    add safeAreaView in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
 import { useTheme } from "styled-components/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 export function Routes() {
   const { colors } = useTheme();
   return (
     <GestureHandlerRootView
       style={{ flex: 1, backgroundColor: colors.background }}
     >
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <AppRoutes />
+        </NavigationContainer>
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
 }
